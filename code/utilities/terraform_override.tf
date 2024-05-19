3590f25efb2a7b02cc5ded1e6e1d594b2c1ad9bd
Marvin Buss <marvin.buss@gmail.com>
2024-05-19 14:37:08 +0200

    Update test config

---
+++ code/utilities/terraform_override.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.103.1"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "1.13.1"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  environment                = "public"
+  skip_provider_registration = false
+  storage_use_azuread        = true
+
+  features {
+    cognitive_account {
+      purge_soft_delete_on_destroy = true
+    }
+    key_vault {
+      purge_soft_delete_on_destroy               = false
+      purge_soft_deleted_certificates_on_destroy = false
+      purge_soft_deleted_keys_on_destroy         = false
+      purge_soft_deleted_secrets_on_destroy      = false
+      recover_soft_deleted_key_vaults            = true
+      recover_soft_deleted_certificates          = true
+      recover_soft_deleted_keys                  = true
+      recover_soft_deleted_secrets               = true
+    }
+    log_analytics_workspace {
+      permanently_delete_on_destroy = false
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azapi" {
+  default_location           = var.location
+  default_tags               = var.tags
+  environment                = "public"
+  skip_provider_registration = false
+}
