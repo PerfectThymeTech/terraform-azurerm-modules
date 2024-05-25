ce2057e4a47aec81b84ecc996189ceb731dfa75b
Marvin Buss <marvin.buss@gmail.com>
2024-05-25 12:15:42 +0200

    Update Storage Module Output Descriptions

--- modules/storage/output.tf
+++ modules/storage/output.tf
@@ -1,48 +1,54 @@
 output "storage_account_id" {
-  description = "Resource of the storage account"
+  description = "Specifies the resource id of the storage account."
   value       = azurerm_storage_account.storage_account.id
   sensitive   = false
 }
 
 output "storage_account_name" {
-  description = "Name"
+  description = "Specifies the resource name of the storage account."
   value       = azurerm_storage_account.storage_account.name
   sensitive   = false
 }
 
 output "storage_account_primary_blob_endpoint" {
-  description = "Primary blob endpoint"
+  description = "Specifies the primary blob endpoint of the storage account."
   value       = azurerm_storage_account.storage_account.primary_blob_endpoint
   sensitive   = false
 }
 
 output "storage_account_primary_file_endpoint" {
-  description = "Primary file endpoint"
+  description = "Specifies the primary file endpoint of the storage account."
   value       = azurerm_storage_account.storage_account.primary_file_endpoint
   sensitive   = false
 }
 
 output "storage_account_primary_queue_endpoint" {
-  description = "Primary queue endpoint"
+  description = "Specifies the primary queue endpoint of the storage account."
   value       = azurerm_storage_account.storage_account.primary_queue_endpoint
   sensitive   = false
 }
 
 output "storage_account_primary_table_endpoint" {
-  description = "Primary table endpoint"
+  description = "Specifies the primary table endpoint of the storage account."
   value       = azurerm_storage_account.storage_account.primary_table_endpoint
   sensitive   = false
 }
 
 output "storage_account_primary_web_endpoint" {
-  description = "Primary web endpoint"
+  description = "Specifies the primary web endpoint of the storage account."
   value       = azurerm_storage_account.storage_account.primary_web_endpoint
   sensitive   = false
 }
 
+output "storage_account_primary_dfs_endpoint" {
+  description = "Specifies the primary dfs endpoint of the storage account."
+  value       = azurerm_storage_account.storage_account.primary_dfs_endpoint
+  sensitive   = false
+}
+
 output "storage_setup_completed" {
-  value       = true
   description = "Specifies whether the connectivity and identity has been successfully configured."
+  value       = true
   sensitive   = false
 
   depends_on = [
