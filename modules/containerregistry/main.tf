bd7b3193a4ada7aa8b2c95a498d370d3104da8b0
Marvin Buss <marvin.buss@gmail.com>
2024-05-27 09:04:48 +0200

    Module consistency updates

--- modules/containerregistry/main.tf
+++ modules/containerregistry/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "container_registry" {
-  name                = replace(var.container_registry_name, "-", "")
+  name                = var.container_registry_name
   location            = var.location
   resource_group_name = var.resource_group_name
   tags                = var.tags
