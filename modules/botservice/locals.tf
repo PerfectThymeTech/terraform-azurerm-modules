23fc2c268a2219bf2ce1cedde9be1060501f98ea
Marvin Buss <marvin.buss@gmail.com>
2024-08-22 17:27:00 +0200

    Fix bug

--- modules/botservice/locals.tf
+++ modules/botservice/locals.tf
@@ -1,6 +1,6 @@
 locals {
   application_insights = {
-    name                = split(var.bot_service_application_insights_id, "/")[8]
-    resource_group_name = split(var.bot_service_application_insights_id, "/")[4]
+    name                = split("/", var.bot_service_application_insights_id)[8]
+    resource_group_name = split("/", var.bot_service_application_insights_id)[4]
   }
 }
