324954db2cca62c9b98f110995e75a2d9e89e3bd
Marvin Buss <marvin.buss@gmail.com>
2024-09-17 14:28:44 +0200

    Add name as output to bot service

--- modules/botservice/outputs.tf
+++ modules/botservice/outputs.tf
@@ -4,6 +4,12 @@ output "bot_service_id" {
   sensitive   = false
 }
 
+output "bot_service_name" {
+  description = "Specifies the name of the bot service."
+  value       = azurerm_bot_service_azure_bot.bot_service_azure_bot.name
+  sensitive   = false
+}
+
 output "bot_service_setup_completed" {
   description = "Specifies whether the connectivity and identity has been successfully configured."
   value       = true
