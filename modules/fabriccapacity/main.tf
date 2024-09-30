f92abcbeb5c0a832cdf4db1b2c903471e8ce7473
Marvin Buss <marvin.buss@gmail.com>
2024-09-30 09:15:39 +0200

    Lint

--- modules/fabriccapacity/main.tf
+++ modules/fabriccapacity/main.tf
@@ -1,10 +1,10 @@
 resource "azapi_resource" "fabric_capacity" {
-  type = "Microsoft.Fabric/capacities@2023-11-01"
-  name = var.fabric_capacity_name
-  location = var.location
+  type      = "Microsoft.Fabric/capacities@2023-11-01"
+  name      = var.fabric_capacity_name
+  location  = var.location
   parent_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}"
   tags      = var.tags
-  
+
   body = {
     properties = {
       administration = {
@@ -16,7 +16,7 @@ resource "azapi_resource" "fabric_capacity" {
       tier = "Fabric"
     }
   }
-  
+
   response_export_values    = []
   schema_validation_enabled = false # Can be reverted once this is closed: https://github.com/Azure/terraform-provider-azapi/issues/524
   locks                     = []
--- modules/fabriccapacity/tests/test.tftest.hcl
+++ modules/fabriccapacity/tests/test.tftest.hcl
@@ -2,14 +2,14 @@ run "create_fabric_capacity" {
   command = apply
 
   variables {
-    location                  = "germanywestcentral"
-    resource_group_name       = "tfmodule-test-rg"
+    location            = "germanywestcentral"
+    resource_group_name = "tfmodule-test-rg"
     tags = {
       test = "fabric-capacity"
     }
-    fabric_capacity_name                         = "tftstr-001"
-    fabric_capacity_admin_email                          = []
-    fabric_capacity_sku          = "F2"
+    fabric_capacity_name        = "tftstr-001"
+    fabric_capacity_admin_email = []
+    fabric_capacity_sku         = "F2"
   }
 
   assert {
