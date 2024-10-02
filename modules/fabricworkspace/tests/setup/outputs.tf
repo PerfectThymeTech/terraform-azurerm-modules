b651782ee29c229589a5944776f4702f0246fb92
Marvin Buss <marvin.buss@gmail.com>
2024-10-02 14:41:55 +0200

    Run another test

--- modules/fabricworkspace/tests/setup/outputs.tf
+++ modules/fabricworkspace/tests/setup/outputs.tf
@@ -3,3 +3,9 @@ output "fabric_capacity_id" {
   value       = module.fabric_capacity.fabric_capacity_id
   sensitive   = false
 }
+
+output "fabric_capacity_name" {
+  description = "Specifies the id of the fabric capacity."
+  value       = module.fabric_capacity.fabric_capacity_name
+  sensitive   = false
+}
