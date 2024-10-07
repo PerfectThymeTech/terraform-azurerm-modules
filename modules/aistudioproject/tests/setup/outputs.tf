a14a9aca3d42e49ad62e98494eadd95298704de8
Marvin Buss <marvin.buss@gmail.com>
2024-10-08 00:48:56 +0200

    Test AI Studio

--- modules/aistudioproject/tests/setup/outputs.tf
+++ modules/aistudioproject/tests/setup/outputs.tf
@@ -3,3 +3,9 @@ output "ai_studio_hub_id" {
   value       = module.ai_studio_hub.ai_studio_hub_id
   sensitive   = false
 }
+
+output "ai_studio_hub_storage_account_id" {
+  description = "Specifies the resource id of ai studio hub."
+  value       = module.storage_account.storage_account_id
+  sensitive   = false
+}
