c1cbfff2db92136f86994dbd768cd5cfe6181751
Marvin Buss <marvin.buss@gmail.com>
2024-11-19 00:20:04 +0100

    Add missing provider for purview

--- modules/purview/terraform.tf
+++ modules/purview/terraform.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 4.0.0"
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.0"
+    }
     time = {
       source  = "hashicorp/time"
       version = ">= 0.9.1"
