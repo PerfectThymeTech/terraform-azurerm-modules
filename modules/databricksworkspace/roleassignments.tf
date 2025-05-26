8156275a94883e49286f2f8a6c083a20820e2666
Marvin Buss <marvin.buss@gmail.com>
2025-05-26 18:57:39 +0200

    Add dependency

--- modules/databricksworkspace/roleassignments.tf
+++ modules/databricksworkspace/roleassignments.tf
@@ -18,3 +18,12 @@ resource "azurerm_role_assignment" "role_assignment_key_vault_crypto_encryption_
   principal_id         = azurerm_databricks_workspace.databricks_workspace.storage_account_identity[0].principal_id
   principal_type       = "ServicePrincipal"
 }
+
+resource "time_sleep" "sleep_cmk" {
+  create_duration = "120s"
+
+  depends_on = [
+    azurerm_role_assignment.role_assignment_key_vault_crypto_encryption_user_databricks_managed_disk_identity,
+    azurerm_role_assignment.role_assignment_key_vault_crypto_encryption_user_databricks_storage_account_identity,
+  ]
+}
