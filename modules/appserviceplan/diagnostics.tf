2502f4ba3954f019a115db3470777138dd21765a
Marvin Buss <marvin.buss@gmail.com>
2025-12-09 12:02:12 +0100

    Fix ASP deprecation warning

--- modules/appserviceplan/diagnostics.tf
+++ modules/appserviceplan/diagnostics.tf
@@ -18,12 +18,11 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_service_plan" 
     }
   }
 
-  dynamic "metric" {
+  dynamic "enabled_metric" {
     iterator = entry
     for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_service_plan.metrics
     content {
       category = entry.value
-      enabled  = true
     }
   }
 }
