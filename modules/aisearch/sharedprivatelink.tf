1e5455b53b48f0032638302263eb65913358a5df
Marvin Buss <marvin.buss@gmail.com>
2026-03-30 10:47:20 +0200

    Add Approval for PEs for Azure Search

--- modules/aisearch/sharedprivatelink.tf
+++ modules/aisearch/sharedprivatelink.tf
@@ -8,3 +8,22 @@ resource "azurerm_search_shared_private_link_service" "search_shared_private_lin
   subresource_name   = each.value.subresource_name
   target_resource_id = each.value.target_resource_id
 }
+
+resource "null_resource" "search_shared_private_link_service_approval" {
+  for_each = var.search_service_shared_private_links
+
+  triggers = {
+    run_once           = "true"
+    name               = azurerm_search_shared_private_link_service.search_shared_private_link_service[each.key].name
+    subresource_name   = azurerm_search_shared_private_link_service.search_shared_private_link_service[each.key].subresource_name
+    target_resource_id = azurerm_search_shared_private_link_service.search_shared_private_link_service[each.key].target_resource_id
+  }
+  provisioner "local-exec" {
+    interpreter = ["pwsh", "-Command"]
+    command     = "./Approve-ManagedPrivateEndpoint.ps1 -ResourceId '${azurerm_search_shared_private_link_service.search_shared_private_link_service[each.key].target_resource_id}' -WorkspaceName '${azurerm_search_service.search_service.name}' -ManagedPrivateEndpointName '${azurerm_search_shared_private_link_service.search_shared_private_link_service[each.key].name}'"
+    on_failure  = fail
+    quiet       = false
+    when        = create
+    working_dir = "${path.module}/scripts/"
+  }
+}
