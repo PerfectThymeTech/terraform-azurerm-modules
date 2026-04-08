aac77687ab5941d2ddeb995cc9d09386adef1a6c
Marvin Buss <marvin.buss@gmail.com>
2026-04-08 13:08:52 +0200

    Add diagnostics for blob, file, queue and table

--- modules/storage/diagnostics.tf
+++ modules/storage/diagnostics.tf
@@ -26,3 +26,119 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_storage_accoun
     }
   }
 }
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_storage_account_blob" {
+  for_each = { for index, value in var.diagnostics_configurations :
+    index => {
+      log_analytics_workspace_id = value.log_analytics_workspace_id,
+      storage_account_id         = value.storage_account_id
+    }
+  }
+  name                       = "applicationLogs-${each.key}"
+  target_resource_id         = local.resource_id_blob
+  log_analytics_workspace_id = each.value.log_analytics_workspace_id == "" ? null : each.value.log_analytics_workspace_id
+  storage_account_id         = each.value.storage_account_id == "" ? null : each.value.storage_account_id
+
+  dynamic "enabled_log" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_storage_account_blob.log_category_groups
+    content {
+      category_group = entry.value
+    }
+  }
+
+  dynamic "enabled_metric" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_storage_account_blob.metrics
+    content {
+      category = entry.value
+    }
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_storage_account_file" {
+  for_each = { for index, value in var.diagnostics_configurations :
+    index => {
+      log_analytics_workspace_id = value.log_analytics_workspace_id,
+      storage_account_id         = value.storage_account_id
+    }
+  }
+  name                       = "applicationLogs-${each.key}"
+  target_resource_id         = local.resource_id_file
+  log_analytics_workspace_id = each.value.log_analytics_workspace_id == "" ? null : each.value.log_analytics_workspace_id
+  storage_account_id         = each.value.storage_account_id == "" ? null : each.value.storage_account_id
+
+  dynamic "enabled_log" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_storage_account_blob.log_category_groups
+    content {
+      category_group = entry.value
+    }
+  }
+
+  dynamic "enabled_metric" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_storage_account_blob.metrics
+    content {
+      category = entry.value
+    }
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_storage_account_table" {
+  for_each = { for index, value in var.diagnostics_configurations :
+    index => {
+      log_analytics_workspace_id = value.log_analytics_workspace_id,
+      storage_account_id         = value.storage_account_id
+    }
+  }
+  name                       = "applicationLogs-${each.key}"
+  target_resource_id         = local.resource_id_table
+  log_analytics_workspace_id = each.value.log_analytics_workspace_id == "" ? null : each.value.log_analytics_workspace_id
+  storage_account_id         = each.value.storage_account_id == "" ? null : each.value.storage_account_id
+
+  dynamic "enabled_log" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_storage_account_table.log_category_groups
+    content {
+      category_group = entry.value
+    }
+  }
+
+  dynamic "enabled_metric" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_storage_account_table.metrics
+    content {
+      category = entry.value
+    }
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_storage_account_queue" {
+  for_each = { for index, value in var.diagnostics_configurations :
+    index => {
+      log_analytics_workspace_id = value.log_analytics_workspace_id,
+      storage_account_id         = value.storage_account_id
+    }
+  }
+  name                       = "applicationLogs-${each.key}"
+  target_resource_id         = local.resource_id_queue
+  log_analytics_workspace_id = each.value.log_analytics_workspace_id == "" ? null : each.value.log_analytics_workspace_id
+  storage_account_id         = each.value.storage_account_id == "" ? null : each.value.storage_account_id
+
+  dynamic "enabled_log" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_storage_account_queue.log_category_groups
+    content {
+      category_group = entry.value
+    }
+  }
+
+  dynamic "enabled_metric" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_storage_account_queue.metrics
+    content {
+      category = entry.value
+    }
+  }
+}
