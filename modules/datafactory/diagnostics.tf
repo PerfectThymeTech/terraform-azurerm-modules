90fb7421b2152553bd42445961053d69411a242b
Marvin Buss <marvin.buss@gmail.com>
2026-04-08 18:19:30 +0200

    Update diagnostics for Data Factory

--- modules/datafactory/diagnostics.tf
+++ modules/datafactory/diagnostics.tf
@@ -18,12 +18,11 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_data_factory" 
     }
   }
 
-  dynamic "metric" {
+  dynamic "enabled_metric" {
     iterator = entry
     for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_data_factory.metrics
     content {
       category = entry.value
-      enabled  = true
     }
   }
 }
