48f2afdb089543821bd3dc377af04d666ed92477
Marvin Buss <marvin.buss@gmail.com>
2026-04-08 09:41:16 +0200

    Update storage diagnostics

--- modules/storage/diagnostics.tf
+++ modules/storage/diagnostics.tf
@@ -18,12 +18,11 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_storage_accoun
     }
   }
 
-  dynamic "metric" {
+  dynamic "enabled_metric" {
     iterator = entry
     for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_storage_account.metrics
     content {
       category = entry.value
-      enabled  = true
     }
   }
 }
