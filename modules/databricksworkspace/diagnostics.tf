40da964191ee0531e781539b54e5cef01b20ac1a
Marvin Buss <marvin.buss@gmail.com>
2026-04-09 13:56:25 +0200

    Update Databricks Workspace Diags

--- modules/databricksworkspace/diagnostics.tf
+++ modules/databricksworkspace/diagnostics.tf
@@ -18,12 +18,11 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_databricks_wor
     }
   }
 
-  dynamic "metric" {
+  dynamic "enabled_metric" {
     iterator = entry
     for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_databricks_workspace.metrics
     content {
       category = entry.value
-      enabled  = true
     }
   }
 }
