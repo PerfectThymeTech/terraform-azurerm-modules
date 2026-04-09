56ce0908e6a563fc25ab94557da225c42acbaa5e
Marvin Buss <marvin.buss@gmail.com>
2026-04-09 13:36:13 +0200

    Update PostgreSql Diagnostics

--- modules/postgresqlflexible/diagnostics.tf
+++ modules/postgresqlflexible/diagnostics.tf
@@ -18,12 +18,11 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_postgresql_fle
     }
   }
 
-  dynamic "metric" {
+  dynamic "enabled_metric" {
     iterator = entry
     for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_postgresql_flexible_server.metrics
     content {
       category = entry.value
-      enabled  = true
     }
   }
 }
