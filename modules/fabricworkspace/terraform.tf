f9e931ae133befbbeb6cda076399ddaddb1ce7c1
Marvin Buss <marvin.buss@gmail.com>
2026-04-15 09:45:31 +0200

    Update provider config for Fabric workspace

--- modules/fabricworkspace/terraform.tf
+++ modules/fabricworkspace/terraform.tf
@@ -2,9 +2,21 @@ terraform {
   required_version = "~> 1.8"
 
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.0"
+    }
     fabric = {
       source  = "microsoft/fabric"
       version = "~> 1.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
     }
   }
 }
